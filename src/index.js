789acafd42c200a322f66bad921fdc077594f007
RafaelZucarelli <zucarelli83@gmail.com>
2024-03-28 16:17:27 -0300

    chore:ChangeContext/ChangeCounter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { CounterContext, CounterContextProvider } from './context/CounterContext';
+import { CounterContextProvider } from './context/CounterContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
